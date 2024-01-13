320a1252adbcd1ce4b4d8e958402b5b9b36f9c84
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2024-01-13 17:44:23 +0600

    dummy

--- Online [TH] Task/String Funcs/tempCodeRunnerFile.cpp
+++ Online [TH] Task/String Funcs/tempCodeRunnerFile.cpp
@@ -2,7 +2,12 @@
 #include<cstring>
 using namespace std;
 int main(){
-   char S1[]="ABBAB";
-   cout<<"String Search:"<<strstr(S1,"BA")<<endl;
+   char S1[]="Hello";
+   char S2[]="World";
+   cout<<"Before S1:"<<S1<<endl;
+   cout<<"Before S2:"<<S2<<endl;
+   cout<<"Concatenation:"<<strcat(S1,S2)<<endl;
+   cout<<"After S1:"<<S1<<endl;
+   cout<<"After S2:"<<S2<<endl;
  
 }
