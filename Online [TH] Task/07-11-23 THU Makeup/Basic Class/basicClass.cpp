cae7647d6d00f1813607040739bf33f8385730da
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2024-06-26 04:40:21 +0600

    s

--- Online [TH] Task/07-11-23 THU Makeup/Basic Class/basicClass.cpp
+++ Online [TH] Task/07-11-23 THU Makeup/Basic Class/basicClass.cpp
@@ -32,7 +32,19 @@ public:
 int main(){
     cout << endl;
     Student S1;
-    S1.Set_StudentInfo("ABC","23-45",1.23);
+    S1.Set_StudentInfo("ABC","23-45",4);
     S1.Get_StudentInfo();
+    S1.cgpa_Info();
+    Student S2;
+    S2.Set_StudentInfo("Siam","23-53004-3",0);
+    S2.Get_StudentInfo();
+    S2.cgpa_Info();
+
+    cout << endl;
+    S1.Get_StudentInfo();
+    S2.cgpa_Info();
+
+    cout << endl;
+    S2.Get_StudentInfo();
     S1.cgpa_Info();
 }
